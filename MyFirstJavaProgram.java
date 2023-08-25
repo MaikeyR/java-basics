39688f5f11039d7c0972f9f482114a9e2098a434
Maikel <mreijneke@student.che.nl>
2023-08-25 21:23:52 +0200

    loops

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -1,5 +1,20 @@
 public class MyFirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello, this is my first java program");
+        System.out.println("");
+
+        for(int i=0; i<5; i++) {
+            System.out.println("Iteration # " + i);
+        }
+        System.out.println("");
+
+        int j=0;
+        while(j<5) {
+            System.out.println("Iteration # " + j);
+            j++;
+        }
+        System.out.println("");
+
+        
     }
 }
