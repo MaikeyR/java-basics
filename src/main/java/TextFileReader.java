4d2caf43b54c7ff579852185cc09da23561b8521
Maikel <mreijneke@student.che.nl>
2023-08-26 22:33:50 +0200

    relative path fix (NEW RUN COMMANDS FROM NOW ON)

--- src/main/java/TextFileReader.java
+++ src/main/java/TextFileReader.java
@@ -1,9 +1,12 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TextFileReader {
+  /**
+   * @param args
+   */
   public static void main(String[] args) {
     InputStreamReader isr = null;
     BufferedReader br = null;
@@ -20,19 +23,15 @@ public class TextFileReader {
       e.printStackTrace();
     } catch (IOException e) {
       System.err.println("Can not read from file!");
-
       e.printStackTrace();
     } finally {
-      if (br != null)
+      if (br != null) {
         try {
           br.close();
         } catch (IOException e) {
-          /* ensure close */ }
-      if (isr != null)
-        try {
-          isr.close();
-        } catch (IOException e) {
-          /* ensure close */ }
+          /* ensure close */
+        }
+      }
     }
   }
 }
