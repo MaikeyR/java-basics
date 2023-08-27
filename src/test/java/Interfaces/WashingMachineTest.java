7ed239e7a56cb4aca06e5c515815c89ae0ab1cb8
Maikel <mreijneke@student.che.nl>
2023-08-27 21:48:53 +0200

    interfaces

---
+++ src/test/java/Interfaces/WashingMachineTest.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public class WashingMachineTest {
+    public static void main(String[] args) {
+        WashingMachine samsungWashingMachine = new SamsungWashingMachine();
+        samsungWashingMachine.startButtonPressed();
+    }
+}
