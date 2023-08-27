e980871ea08182df4334c786e4581e6c711398a5
Maikel <mreijneke@student.che.nl>
2023-08-27 02:23:15 +0200

    tests and teststructure

---
+++ src/main/java/ObjectsAndClasses/Car.java
@@ -0,0 +1,16 @@
+package ObjectsAndClasses;
+
+public class Car {
+    
+    int currentSpeed;
+    String name;
+    
+    public void accelerate() {    
+    }
+    
+    public void park() {
+    }
+    
+    public void printCurrentSpeed() {
+    }
+}
