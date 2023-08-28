9dbb868f89f3d39fe6c9952852f8a771ac51ed9b
Maikel <mreijneke@student.che.nl>
2023-08-29 01:02:34 +0200

    Polymorphism

---
+++ src/main/java/Polymorphism/PolymorphismExample.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+public class PolymorphismExample {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();
+        Dog dog = new Dog();
+        dog.makeSound();
+        animal = new Cat();
+        animal.makeSound();
+    }
+
+}
