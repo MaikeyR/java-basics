4c807ceaa6ecbfadeeb939eecf21ed66e3dd5fb7
Maikel <mreijneke@student.che.nl>
2023-08-29 22:20:03 +0200

    memorymodel 2

--- src/main/java/MemoryModel/HeapMemoryExample.java
+++ src/main/java/MemoryModel/HeapMemoryExample.java
@@ -5,5 +5,9 @@ public class HeapMemoryExample {
         int id = 1;
         String pName = "Rick";
         Person p = new Person(id, pName); // Instance created in Heap memory
+        
+        System.out.println("Person created:");
+        System.out.println("PID: " + p.pid);
+        System.out.println("Name: " + p.name);
     }
 }
