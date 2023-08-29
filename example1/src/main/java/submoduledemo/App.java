1a685793fd02c681d5fd6fbc8617016a4d97e870
Maikel <mreijneke@student.che.nl>
2023-08-29 17:10:18 +0200

    submodule setup test

---
+++ example1/src/main/java/submoduledemo/App.java
@@ -0,0 +1,13 @@
+package submoduledemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
